5c4e711bed90721195f67931fc9183da8f988603
Jayasawar1325 <rautjayasawar1325@gmail.com>
2025-03-03 21:52:32 +0545

    routers and controllers

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,8 +1,27 @@
 const asyncHandler = (requestHandler) => {
-    return (req, res, next) => {  // Return a new function that Express can use as middleware
-        Promise.resolve(requestHandler(req, res, next)) // Resolve the handler function
-            .catch((err) => next(err)); // Catch errors and pass them to Express error handler
-    };
-};
+    return (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err))
+    }
+}
 
-export { asyncHandler };
+
+export { asyncHandler }
+
+
+
+
+// const asyncHandler = () => {}
+// const asyncHandler = (func) => () => {}
+// const asyncHandler = (func) => async () => {}
+
+
+// const asyncHandler = (fn) => async (req, res, next) => {
+//     try {
+//         await fn(req, res, next)
+//     } catch (error) {
+//         res.status(err.code || 500).json({
+//             success: false,
+//             message: err.message
+//         })
+//     }
+// }
